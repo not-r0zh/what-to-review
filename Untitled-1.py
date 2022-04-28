f7194c81f41548c475688590f163feccae272c91
r0zh <roberuto.kun.16@gmail.com>
2022-04-28 08:42:41 +0200

    Bug fixed

--- Untitled-1.py
+++ Untitled-1.py
@@ -17,19 +17,17 @@ for i in range(len(list)):
                 ## cont2 = numero de lineas que hay hasta la siguiente con ##
                 cont2 += 1
                 cont += 1
-        ## En canso de que no haya ninguna linea más con ##
+        ## En caso de que no haya ninguna linea más con ##
         except:
             print("Se acabo el texto")
         ## Le pongo el Heading
         print(cont)
         list_with_wrongs.append(list[i])
         for x in range(cont2):
-            ## Si hay una sola línea que tenga algo mal poner el apartado entero
+            ## Si hay una sola línea, que tenga algo mal poner el apartado entero
             if list[i+x+1].find('mark') != -1:
                 for z in range(cont2):
                     list_with_wrongs.append(list[i+z+1])
-                break
-            else:
                 break
 
 full_text= ""
